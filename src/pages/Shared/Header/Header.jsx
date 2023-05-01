cdab6af3c027b5de6143ae2788d8943fbd399e8f
ShamimHossain1 <beastb697@gmail.com>
2023-05-01 23:41:04 +0600

    mommentjs

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import logo from '../../../assets/logo.png'
+import moment from 'moment';
 
 const Header = () => {
     return (
-        <div>
-            <h2>This is header</h2>
+        <div className='my-5'>
+            <div className='text-center'>
+                <img src={logo} className='mx-auto' alt="" />
+                <h2><small className='text-lg font-semibold text-slate-500'>Journalism Without Fear or Favour</small></h2>
+                <p className='text-lg font-semibold text-slate-600'>{moment().format("dddd, MMMM Do, YYYY")}</p>
+
+
+            </div>
         </div>
     );
 };
